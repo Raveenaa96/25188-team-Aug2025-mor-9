69d7431d4e916d7e150339ab0a7f50313bc04af8
saravanan.mca1992@gmail.com <53892651+saravanan-prog@users.noreply.github.com>
2025-11-11 09:32:12 +0530

    class_component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  HelloWorld  from './pages/react-jsx/Helloworld';
-import IlovejsxMessage from './pages/react-jsx/IlovejsxMessage';
-import Calculation from './pages/react-data-Binding/Calculation';
-import Conditon from './pages/react-conditons/Condition';
-import Looping from './pages/react-loop/Looping';
+import ExampleClassComponent from './pages/react-components/class_component/ExampleClassComponent';
 
 
 
@@ -16,11 +12,8 @@ import Looping from './pages/react-loop/Looping';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      {/* <HelloWorld />
-      <IlovejsxMessage /> */}
-      {/* <Calculation /> */}
-      {/* <Conditon /> */}
-      <Looping />
+      
+      <ExampleClassComponent />
 
     </React.StrictMode>
    
--- src/pages/react-jsx/IlovejsxMessage.jsx
+++
@@ -1,7 +0,0 @@
-export default function IlovejsxMessage() {
-  return (
-    <div>
-      <h2> I love Jsx code very much </h2>
-    </div>
-  );
-}
