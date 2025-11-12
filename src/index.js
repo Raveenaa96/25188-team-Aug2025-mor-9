b6b7c02a3e4292a6c1e7398898d8a93a8729dee5
jesinthap <jesinthap426@gmail.com>
2025-11-12 10:34:05 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import Welcome from './React_pages/React-jsx/Welcome';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Looping from './React_pages/React-loop/Looping';
+import Calculation from './React_pages/React-data-Binding/Calculation';
+import Conditon from './React_pages/react-conditions/Condition';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Welcome/>
+    <Looping/>
+    <Calculation/>
+    <Conditon/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
