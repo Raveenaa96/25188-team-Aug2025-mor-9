1a8dd7fd34f98a4e12b4813c30f6b376bccc6e4e
laxmiroshini <laxmiroshini@gmail.com>
2025-11-13 23:57:09 +0530

    react-components-events-state management

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ConstructorImplementation from "./components/class_component/ConstructorImplementation";
+import InsituteProfile from "./components/class_component/InstituteProfile";
+import ReactDOM from "react-dom/client";
+import MovieDetails from "./state/fc/MovieDetails";
+import SchoolManagement from "./state/cc/SchoolManagement";
+import MovieCard from "./components/function_component/MovieCard";
+import FoodMenu from "./state/cc/FoodMenu";
+import Employee from "./state/cc/Employee";
+import EmployeeManagement from "./state/fc/EmployeeManagement";
+import SchoolManagementSystem from "./state/fc/SchoolManagementSystem";
+import ButtonOnClick from "./events/ButtonOnClick";
+import InputOnChange from "./events/InputOnChange";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h4>FSD-RJS SARAVANAN BATCH</h4>
-    <ol>
-      <li>Laxmi Roshini</li>
-      <li>Pavithra</li>
-      <li>Jesintha</li>
-      <li>Shenbagam</li>
-      <li>Prem Kumar</li>
-      <li>Marimuthu</li>
-      <li>Shreepal</li>
-    </ol>
+    <ButtonOnClick/>
+    <InputOnChange/>
+    <SchoolManagementSystem/>
+    <FoodMenu/>
+    <EmployeeManagement/>
+    <MovieDetails/>
+    <MovieCard />
+    <Employee/>
+    <InsituteProfile />
+    <SchoolManagement/>
+    <ConstructorImplementation/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
