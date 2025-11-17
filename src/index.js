027f775f8e9a4a2d05f97b870c9628ec6ede0f59
saravanan.mca1992@gmail.com <53892651+saravanan-prog@users.noreply.github.com>
2025-11-17 09:10:51 +0530

    "index file update"

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       
+      
       {/* <Countercc /> */}
       {/* <InputChangeEventcc /> */}
       {/* <MultipleObjectStateUpdatecc /> */}
